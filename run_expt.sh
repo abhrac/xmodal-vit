ef38d3f402d78a249f263bf7d7f301d87ac44dcb
abhrac <abhrachaudhuri.97@gmail.com>
2022-11-20 14:37:24 +0000

    initial commit

---
+++ run_expt.sh
@@ -0,0 +1,4 @@
+python src/train_teacher.py --dataset=ShoeV2
+python src/train_photo_student.py --dataset=ShoeV2
+python src/train_sketch_student.py --dataset=ShoeV2
+python src/test.py --dataset=ShoeV2
